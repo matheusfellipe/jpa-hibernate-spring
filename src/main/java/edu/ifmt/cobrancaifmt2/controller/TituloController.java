fd9d8cefa8dd87c7ea3e915fb5d5b274effe17e6
Matheus Fellipe <matheus060399@gmail.com>
2022-10-26 21:26:36 -0300

    my first commit

--- src/main/java/edu/ifmt/cobrancaifmt2/controller/TituloController.java
+++ src/main/java/edu/ifmt/cobrancaifmt2/controller/TituloController.java
@@ -5,25 +5,31 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+import edu.ifmt.cobrancaifmt2.modelo.StatusTitulo;
 import edu.ifmt.cobrancaifmt2.modelo.Titulo;
 import edu.ifmt.cobrancaifmt2.repository.Titulos;
 
 @Controller
 @RequestMapping("/titulos")
 public class TituloController {
-	
+
 	@Autowired
 	private Titulos titulos;
-	
+
 	@GetMapping("/novo")
-	public String novo() {
-		return "CadastroTitulo";
+	public ModelAndView novo() {
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		mv.addObject("todosStatusTitulo", StatusTitulo.values());
+		return mv;
 	}
-	
-	@RequestMapping(method=RequestMethod.POST)
-	public String salvar(Titulo titulo) {
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ModelAndView salvar(Titulo titulo) {
 		titulos.save(titulo);
-		return "CadastroTitulo";
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		mv.addObject("mensagem", "Foi salvo sucesso");
+		return mv;
 	}
 }
